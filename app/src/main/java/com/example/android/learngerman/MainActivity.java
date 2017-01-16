ceaea5876f2cab1e3bfffc263b99d2c97da04e75
aniketrd <aniketrd@gmail.com>
2017-01-16 21:16:56 +0530

    Add empty layouts for Number, family, colors, phrases

--- app/src/main/java/com/example/android/learngerman/MainActivity.java
+++ app/src/main/java/com/example/android/learngerman/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.learngerman;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openNumberActivity(View view)
+    {
+        Intent intent = new Intent(this,NumberActivity.class);
+        startActivity(intent);
+    }
+    public void openColorsActivity(View view)
+    {
+        Intent intent = new Intent(this,ColorsActivity.class);
+        startActivity(intent);
+    }
+    public void openPhrasesActivity(View view)
+    {
+        Intent intent = new Intent(this,PhraseActivity.class);
+        startActivity(intent);
+    }
+
+    public void openFamilyActivity(View view)
+    {
+        Intent intent = new Intent(this,FamilyActivity.class);
+        startActivity(intent);
     }
 }
