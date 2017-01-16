75bbfb9c1f3f3ddb77e60491efd0d2354c80172b
aniketrd <aniketrd@gmail.com>
2017-01-16 23:36:26 +0530

    Add OnClickListeners to different text view in Main Activity

--- app/src/main/java/com/example/android/learngerman/MainActivity.java
+++ app/src/main/java/com/example/android/learngerman/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +13,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        TextView numbersTv = (TextView) findViewById(R.id.tvNumbers);
+        TextView phrasesTv = (TextView) findViewById(R.id.tvPhrases);
+        TextView familyTv = (TextView) findViewById(R.id.tvFamily);
+        TextView colorsTv = (TextView) findViewById(R.id.tvColors);
+        numbersTv.setOnClickListener(new View.OnClickListener(){
 
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Open Numbers List",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        familyTv.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Open Family List",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        colorsTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Open Colors List",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        phrasesTv.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Open Phrase List",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+/*
     public void openNumberActivity(View view)
     {
         Intent intent = new Intent(this,NumberActivity.class);
@@ -34,4 +70,5 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this,FamilyActivity.class);
         startActivity(intent);
     }
+*/
 }
